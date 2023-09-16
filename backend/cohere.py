da01927de1d5acd381154fe2dc17ef9b2b2bbe3f
Andrew Mascillaro <amascillaro@gmail.com>
2023-09-16 00:42:13 -0400

    Add pdf to text functionality

---
+++ backend/cohere.py
@@ -0,0 +1,12 @@
+import pdfplumber
+
+def pdf_to_text(pdf_file: str):
+    """Extract text and statement dates from the PDF"""
+
+    with pdfplumber.open(pdf_file) as pdf:
+        all_text = "\n".join(
+            page.extract_text(x_tolerance=1)
+            for page in pdf.pages
+        )
+    
+    return all_text
