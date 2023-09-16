bad4a48373f961f50770784a30fd4c86e3d167ab
TheWorldWinner <132176049+TheWorldWinner@users.noreply.github.com>
2023-09-16 13:09:06 -0400

    Co-authored-by: Kevin Wang <kevin1015wang@users.noreply.github.com>

--- frontend/script.js
+++ frontend/script.js
@@ -1,8 +1,25 @@
-document.querySelector(".FileButton").addEventListener('dragover', (e) => {
-    e.preventDefault()
-});
-document.querySelector(".FileButton").addEventListener('drop', (e) => {
-    document.getElementById('file').files = e.dataTransfer.files;
-    e.preventDefault()
-});
+function handleFileSelect(evt) {
+    evt.stopPropagation();
+    evt.preventDefault();
 
+    var files = evt.dataTransfer.files; // FileList object.
+    var file = files[0]; 
+    if (file.type.match('application/pdf')) {
+      // Do something with the PDF file 
+    } else {
+      alert('Please drop a PDF file.');
+    }
+  }
+
+  function handleDragOver(evt) {
+    evt.stopPropagation();
+    evt.preventDefault();
+    evt.dataTransfer.dropEffect = 'copy'; 
+  }
+
+  // Setup the dnd listeners.
+  window.addEventListener('DOMContentLoaded', (event) => {
+    var dropZone = document.getElementById('drop_zone');
+    dropZone.addEventListener('dragover', handleDragOver, false);
+    dropZone.addEventListener('drop', handleFileSelect, false);
+  });
