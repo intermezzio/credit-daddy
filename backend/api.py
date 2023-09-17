cc3cbe56dd78f36510d8ea54dccf8d69e12e9e32
Andrew Mascillaro <amascillaro@gmail.com>
2023-09-16 22:14:07 -0400

    Actual post requests this time

--- backend/api.py
+++ backend/api.py
@@ -9,9 +9,9 @@ from icecream import ic
 
 from werkzeug.utils import secure_filename
 
-import cohere_script
-import db_connect
-from PDFReader import pdf_to_text
+# import cohere_script
+# import db_connect
+# from PDFReader import pdf_to_text
 
 
 UPLOAD_FOLDER = "../cache"
@@ -21,22 +21,22 @@ app = Flask(__name__)
 app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
 # CORS(app)
 
-@app.route("/card/<id_>")
-def get_card(id_, methods=["GET"]):
+@app.route("/card/<id_>", methods=["GET"])
+def get_card(id_):
     # get card info
     card_info = db_connect.get_card(id_)
     return card_info
 
 
-@app.route("/chats/<id_>")
-def get_chats(id_, methods=["GET"]):
+@app.route("/chats/<id_>", methods=["GET"])
+def get_chats(id_):
     # get chats
     chats = db_connect.get_chats(id_)
     return chats
 
 
-@app.route("/ask/<id_>/<question>")
-def get_answer(id_, question, methods=["GET"]):
+@app.route("/ask/<id_>/<question>", methods=["GET"])
+def get_answer(id_, question):
     contract = db_connect.get_contract(id_)
     answer = cohere_script.ask_more_card_details(id_, question)
 
@@ -44,44 +44,44 @@ def get_answer(id_, question, methods=["GET"]):
     return answer
 
 
-@app.route("/upload-card")
-# @cross_origin()
-def analyze_contract(methods=["GET", "POST"]):
-    ic(request.form)
-    ic(request.files)
-    ic(request.filename)
-    sys.stdout.flush()
-    name = request.form["name"]
-    if "file" not in request.files or request.filename == "":
-        pass
+@app.route("/upload-card", methods=["GET", "POST"])
+def analyze_contract():
+    if request.method == "POST":
+        ic(request.form)
+        ic(request.files)
+        ic(request.filename)
+        sys.stdout.flush()
+        name = request.form["name"]
+        if "file" not in request.files or request.filename == "":
+            pass
 
-    if request.filename.rsplit(".", 1)[1].lower() not in ALLOWED_EXTENSIONS:
-        pass
+        if request.filename.rsplit(".", 1)[1].lower() not in ALLOWED_EXTENSIONS:
+            pass
 
-    # see if we already have it
-    id_ = db_connect.name_to_id(name)
-    if db_connect.get_card(id_):
-        pass
+        # see if we already have it
+        id_ = db_connect.name_to_id(name)
+        if db_connect.get_card(id_):
+            pass
 
-    filename = secure_filename(file.filename)
-    abs_filename = os.path.join(app.config["UPLOAD_FOLDER"], filename)
-    file.save(abs_filename)
+        filename = secure_filename(file.filename)
+        abs_filename = os.path.join(app.config["UPLOAD_FOLDER"], filename)
+        file.save(abs_filename)
 
-    # upload pdf and convert to text
-    if abs_filename.endswith(".pdf"):
-        contract_text = pdf_to_text(abs_filename)
-    elif abs_filename.endswith(".txt"):
-        with open(abs_filename) as infile:
-            contract_text = infile.read().strip()
+        # upload pdf and convert to text
+        if abs_filename.endswith(".pdf"):
+            contract_text = pdf_to_text(abs_filename)
+        elif abs_filename.endswith(".txt"):
+            with open(abs_filename) as infile:
+                contract_text = infile.read().strip()
 
-    # text to results
-    card_details = cohere_script.extract_card_details(contract_text)
+        # text to results
+        card_details = cohere_script.extract_card_details(contract_text)
 
-    # store results in firebase
-    db_connect.upload_card(name=name, contract=contract_text, **card_details)
+        # store results in firebase
+        db_connect.upload_card(name=name, contract=contract_text, **card_details)
 
-    # return new id
-    return id_
+        # return new id
+        return id_
 
 
 @app.route("/recommend", methods=["GET"])
